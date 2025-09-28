295ff858d859a50d9ce3285bb56966ab1ac6dbe2
Malik Abdullah <abdullahkaleem2004@gmail.com>
2025-09-28 11:04:58 +0500

    injected products with javascript in home changes in css , home dropdown button div added

--- cart-home.js
+++ cart-home.js
@@ -1,30 +1,78 @@
-// const show_menu = document.querySelector(".dropbtn")
-// const show_content = document.querySelector(".dropdown-content")
-// show_menu.addEventListener("click", ()=>{
-//     if(show_content.className = "dropdown-content"){
-//         show_content.className = "dropdown-content-show"
-//     }
-//     else{
-//         show_content.className = "dropdown-content"
-//     }
-// })
+const row1 = document.getElementById("row1")
 
 
-// const link_button = document.querySelectorAll(".nav-link")
+// dropdown  toggle ----------------
+const showdiv = document.querySelector(".catogries")
+const catbtn = document.getElementById("dropdown-button")
 
 
-// link_button.forEach(link=>{
-//     link.addEventListener("click",(e)=>{
-//         e.preventDefault()
-//         console.log(link.textContent + " clicked")
-//         if(window.location === "cart-home.html"){
-//             window.location === "cart.html"
-//         }
-//         else{
-//             window.location === "cart-home.html"
-//         }
-//     });
-// });
+const products = [
+                    {id:1 , name:"S25 Ultra", image: "Images/cart-pic2.jpg", price:2000,  category:"mobile"},
+                    {id:1 , name:"S25 Ultra", image: "Images/cart-pic2.jpg", price:2000,  category:"mobile"},
+                    {id:1 , name:"S25 Ultra", image: "Images/cart-pic2.jpg", price:2000,  category:"mobile"},
+                    {id:1 , name:"S25 Ultra", image: "Images/cart-pic2.jpg", price:2000,  category:"mobile"},
+                    {id:1 , name:"S25 Ultra", image: "Images/cart-pic2.jpg", price:2000,  category:"mobile"},
+                    {id:1 , name:"S25 Ultra", image: "Images/cart-pic2.jpg", price:2000,  category:"mobile"}
+];
 
+products.forEach((product) =>
+    {
+        const productContainer = document.createElement("div");
+        productContainer.className = "obj-col";
+
+        productContainer.innerHTML = `
+            <img src="${product.image}" alt="${product.name}" width="170px"  height="112px"/>
+            <h5>${product.name}</h5>    
+            <p>Rs.$${product.price.toFixed(2)}</p>
+            <div class="cart-btn-container"> <button class="cart-btn">Add to cart</button></div>
+            `
+
+        row1.appendChild(productContainer)
+
+});
+
+
+// row1.innerHTML = values
+
+// console.log(values)
+// console.log(products)
+
+
+// function displayproducts(){
+
+
+    // row1.innerHTML = `
+    //                     <div class="obj-col">
+    //                         <img> ${products.image}</img>
+    
+    //                     </div>
+    
+    // `
+
+
+// }
+
+// displayproducts()
+
+
+// let list = [1,3,4]
+
+// let data = list.forEach((val)=>{
+//     console.log(val)
+// }
+// )
+
+// console.log(data)
+
+
+
+// Dropdown toggle --------------------------
+
+catbtn.addEventListener("click", ()=>{
+
+    showdiv.classList.toggle("show");
+
+
+});
 
 
