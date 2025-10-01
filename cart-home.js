442328b08f130bb4e859c1d8aec0e0fc778de037
Malik Abdullah <abdullahkaleem2004@gmail.com>
2025-10-01 22:11:45 +0500

    category option toggle functionality added

--- cart-home.js
+++ cart-home.js
@@ -1,21 +1,32 @@
-const row1 = document.getElementById("row1")
+// ----------------------------------Constants
 
+const row1 = document.getElementById("row1")
+const mobile = document.querySelector(".mobile")
 
 // dropdown  toggle ----------------
 const showdiv = document.querySelector(".catogries")
 const catbtn = document.getElementById("dropdown-button")
 
-
+// --------------------------------------Products---------------
 const products = [
-                    {id:1 , name:"S25 Ultra", image: "Images/cart-pic2.jpg", price:2000,  category:"mobile"},
-                    {id:1 , name:"S25 Ultra", image: "Images/cart-pic2.jpg", price:2000,  category:"mobile"},
-                    {id:1 , name:"S25 Ultra", image: "Images/cart-pic2.jpg", price:2000,  category:"mobile"},
-                    {id:1 , name:"S25 Ultra", image: "Images/cart-pic2.jpg", price:2000,  category:"mobile"},
-                    {id:1 , name:"S25 Ultra", image: "Images/cart-pic2.jpg", price:2000,  category:"mobile"},
-                    {id:1 , name:"S25 Ultra", image: "Images/cart-pic2.jpg", price:2000,  category:"mobile"}
+                    {id:1 , name:"S25 Ultra",            image: "Images/cart-pic2.jpg", price:2000,  category:"mobile"},
+                    {id:2 , name:"Iphone 15",            image: "Images/cart-pic2.jpg", price:1000,  category:"mobile"},
+                    {id:3 , name:"Redmi 13",             image: "Images/cart-pic2.jpg", price:900,  category:"mobile"},
+                    {id:4 , name:"Poco x3",              image: "Images/cart-pic2.jpg", price:500,  category:"mobile"},
+                    {id:5 , name:"Xpro magic",           image: "Images/cart-pic2.jpg", price:3000,  category:"laptop"},
+                    {id:6 , name:"Mackbook pro",         image: "Images/cart-pic2.jpg", price:2000,  category:"laptop"},
+                    {id:7 , name:"legion x3",            image: "Images/cart-pic2.jpg", price:1500,  category:"laptop"},
+                    {id:8 , name:"Watch Ultra 5 ",       image: "Images/cart-pic2.jpg", price:200,  category:"watch"},
+                    {id:9 , name:"Samsung gear S3",      image: "Images/cart-pic2.jpg", price:200,  category:"watch"},
+                    {id:10, name:"Xioami 3 ",            image: "Images/cart-pic2.jpg", price:190,  category:"watch"}
 ];
 
-products.forEach((product) =>
+
+// --------------------------------------display function---------------
+
+function display(products){
+    // const mobile = products.filter(item => item.category === "mobile")
+    products.forEach((product) =>
     {
         const productContainer = document.createElement("div");
         productContainer.className = "obj-col";
@@ -24,45 +35,73 @@ products.forEach((product) =>
             <img src="${product.image}" alt="${product.name}" width="170px"  height="112px"/>
             <h5>${product.name}</h5>    
             <p>Rs.$${product.price.toFixed(2)}</p>
-            <div class="cart-btn-container"> <button class="cart-btn">Add to cart</button></div>
+            <div class="cart-btn-container"> <button class="cart-btn"  data-id="${product.id}">Add to cart</button></div>
             `
 
         row1.appendChild(productContainer)
 
+});    
+}
+display(products)
+
+// --------------------------------------Render Function ---------------
+
+function display_render() {
+  row1
+    .querySelectorAll(".obj-col")
+    .forEach((item) => item.remove());
+}
+
+
+//  DROp down menu click checker and function  : ---------------------------------
+
+showdiv.addEventListener("click", (event)=>{
+    if(event.target.classList.contains("mobile")){
+        // console.log("clicked" , event.target);
+        const mobile = products.filter(item => item.category === "mobile")
+        display_render()
+        display(mobile)
+        // dropdowntoggle(mobile)
+        
+    }
+
+    else if (event.target.classList.contains("all")){
+        // console.log("clicked" , event.target);
+        display_render()
+        display(products)
+    }
+    
+    else if (event.target.classList.contains("laptop")){
+        // console.log("clicked" , event.target);
+        const laptop = products.filter(item => item.category === "laptop")
+        display_render()
+        display(laptop)
+    }
+
+    else if (event.target.classList.contains("watch")){
+        // console.log("clicked" , event.target);
+        const watch = products.filter(item => item.category === "watch")
+        display_render()
+        display(watch)
+    }
+
+    
 });
 
+// Add to cart function  --------------------------
+row1.addEventListener("click", (event)=>{
+    if(event.target.classList.contains("cart-btn")){
+        const productid = parseInt(event.target.dataset.id)
+        // const objid =  products.find((item)=> item.id === productid)
+        console.log("ID == ",productid)
+    }
+});
 
-// row1.innerHTML = values
-
-// console.log(values)
-// console.log(products)
-
-
-// function displayproducts(){
-
-
-    // row1.innerHTML = `
-    //                     <div class="obj-col">
-    //                         <img> ${products.image}</img>
-    
-    //                     </div>
-    
-    // `
-
+// function addtocart(){
 
 // }
 
-// displayproducts()
 
-
-// let list = [1,3,4]
-
-// let data = list.forEach((val)=>{
-//     console.log(val)
-// }
-// )
-
-// console.log(data)
 
 
 
